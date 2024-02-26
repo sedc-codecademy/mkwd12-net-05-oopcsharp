62e39e4c387d057f49e244cc6a47fc9c2fb48c9a
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2024-02-26 20:54:44 +0100

    [G3] Class 01

---
+++ G3/Class01/FirstProjectApp/FirstProjectApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstProjectApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
