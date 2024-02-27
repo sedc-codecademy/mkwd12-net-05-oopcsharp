7be69edd95b9a2f26578f72f95fe03e57a07e56b
tstojanovska <stojanovskatanja03@gmail.com>
2024-02-27 20:48:05 +0100

    Class 01

---
+++ G7/Class01/Code/QinshiftAcademy.TopLevelStatementConsoleApp/QinshiftAcademy.TopLevelStatementConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("We will try to create a triangle");
+Console.WriteLine("    *");
+Console.WriteLine("   ***");
+Console.WriteLine("  *****");
