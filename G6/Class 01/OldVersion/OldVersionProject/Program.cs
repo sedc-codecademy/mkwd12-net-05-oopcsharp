a70eb55063f25dd5bc215c09a97dd9398d26657e
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-02-27 20:55:19 +0100

    Class 01

---
+++ G6/Class 01/OldVersion/OldVersionProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OldVersionProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, G6!");
+        }
+
+    }
+}
