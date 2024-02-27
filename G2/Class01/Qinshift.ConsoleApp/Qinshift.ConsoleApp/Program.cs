97280831f0f03f004c2e5d68bd0536f3a450a9f3
Danilo Borozan <daniloborozan07@gmail.com>
2024-02-27 20:26:00 +0100

    add code

---
+++ G2/Class01/Qinshift.ConsoleApp/Qinshift.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+Console.Write("Hello G2");
+Console.Write("Hello G2");
