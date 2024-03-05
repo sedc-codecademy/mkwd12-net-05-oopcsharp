15357222112cba5b3789a898dec9f7136756f8e2
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-03-05 20:48:40 +0100

    Class 03

---
+++ G6/Class 03/Code/Exercise03/Program.cs
@@ -0,0 +1,34 @@
+﻿//Get an input from the console
+//Print all numbers from 1 to that number
+//Don’t print numbers that can be divided by 3 or 7
+//If the number gets to 100 print a message: The limit is reached and stop counting
+
+
+Console.WriteLine("Enter a number:");
+bool success = int.TryParse(Console.ReadLine(), out int number);
+
+if (success)
+{
+    for(int i =1; i <= number; i++)
+    {
+        if(i%3 == 0 || i%7 == 0)
+        {
+            continue;
+        }
+
+        Console.WriteLine(i);
+
+        if (i == 100)
+        {
+            Console.WriteLine("The limit is reached");
+            break;
+
+        }
+
+      
+    }
+}
+else
+{
+    Console.WriteLine("Wrong input!");
+}
