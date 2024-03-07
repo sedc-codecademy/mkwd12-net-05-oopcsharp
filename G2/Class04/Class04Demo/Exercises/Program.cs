c4753d249b5bf8ad00a50b3fe839221c3eb2b547
Ilija Mitev <ilija_mitev_mk@yahoo.com>
2024-03-07 18:44:28 +0100

    Class 4 init

---
+++ G2/Class04/Class04Demo/Exercises/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("============== EXERCISES ==============");
+
+#region Exercise 1
+static int Sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+static int Subtract(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+static void Exercise1()
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.Write("Enter operation:");
+        string operation = Console.ReadLine();
+        if (operation != "+" && operation != "-")
+        {
+            Console.WriteLine("Sorry! Wrong operation selected! Try again.");
+            Console.ReadLine();
+            continue; // prevent the code from further execution and go to next iteration
+        }
+
+        Console.Write("Enter first number:");
+        bool isFirstNumValid = int.TryParse(Console.ReadLine(), out int parsedNumOne);
+        Console.Write("Enter second number:");
+        bool isSecondNumValid = int.TryParse(Console.ReadLine(), out int parsedNumTwo);
+
+        if (!isFirstNumValid || !isSecondNumValid)
+        {
+            Console.WriteLine("You've entered invalid numbers!!! Try again.");
+            Console.ReadLine();
+            continue; // prevent the code from further execution and go to next iteration
+        }
+
+        if (operation == "+")
+        {
+            Console.WriteLine(Sum(parsedNumOne, parsedNumTwo));
+        }
+        else if (operation == "-")
+        {
+            Console.WriteLine(Subtract(parsedNumOne, parsedNumTwo));
+        }
+
+        break; // when everything is done, exit the loop
+    }
+    Console.WriteLine("Everything is done :)");
+}
+Exercise1();
+
+#endregion
