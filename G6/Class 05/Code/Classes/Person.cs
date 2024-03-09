8e2b14f49f49d06abd70a7b3c657c73ada7f278f
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-03-09 12:36:43 +0100

    Class 05

---
+++ G6/Class 05/Code/Classes/Person.cs
@@ -0,0 +1,71 @@
+﻿namespace Classes //the path from our sln to our class => in this case it is the project Classes
+{
+    public class Person //the default is internal
+    {
+        //we can access FIrstName, LastName and Age from anywhere
+        public string FirstName { get; set; } //getters and setters
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        //we can access _ssn only from Person class (and its methods)
+        private long _ssn {  get; set; }
+
+        //Constructor
+        //Construcor is always public and has the same name as the class
+        //we need it in order to instantiate objects
+        public Person()
+        {
+            _ssn = new Random().Next(100000, 999999); //gets a random number from 100000 to 999999
+        }
+
+        public Person(string firstName, string lastName, int age, long ssn)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            if(ssn >=100000 && ssn <= 999999)
+            {
+                _ssn = ssn;
+            }
+           
+        }
+
+        //we can create more constructors with different params
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = 0;
+            _ssn = new Random().Next(100000, 999999);
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Firstname: {FirstName} LastName = {LastName} Age: {Age} SSN: {_ssn}");
+        }
+
+        public void Says(string message)
+        {
+            Console.WriteLine($"{FirstName} {LastName} says {message}");
+        }
+
+        //we can use this way to get the value of _ssn from outside
+        public long GetSSN()
+        {
+            return _ssn;
+        }
+
+        //we can set the ssn value from outside, but in a controlled way
+        public void SetSSN(long ssn)
+        {
+            if (ssn >= 100000 && ssn <= 999999)
+            {
+                _ssn = ssn;
+            }
+            else
+            {
+                Console.WriteLine("Invalid ssn value");
+            }
+        }
+    }
+}
---
+++ G6/Class 05/Code/DateTimes/Program.cs
@@ -0,0 +1,82 @@
+﻿//====DATE TIMES========
+
+DateTime emptyDateTime = new DateTime();
+Console.WriteLine(emptyDateTime);
+
+//create DateTime with current Date and time (local date and time, from the server)
+DateTime currentDateTime = DateTime.Now;
+Console.WriteLine("current date time: "+currentDateTime);
+
+DateTime specificDate = new DateTime(1999, 07, 28);
+Console.WriteLine(specificDate);
+
+//year month day hour minute seconds
+DateTime anotherDateTime = new DateTime(2021, 9, 6, 12, 34, 45);
+Console.WriteLine(anotherDateTime);
+
+//parse the date string. pass a string in the format for dates from the server
+//ex: on my laptop is month/day/year
+DateTime parsedDate = DateTime.Parse("03/26/2024");
+//DateTime wrongFormatParsedDate = DateTime.Parse("26/03/2024"); //this won't work on my server (my laptop)
+Console.WriteLine(parsedDate);
+//Console.WriteLine(wrongFormatParsedDate)
+
+DateTime universalDateTime = DateTime.UtcNow;
+Console.WriteLine("utc: "+ universalDateTime);
+
+int day = currentDateTime.Day;
+Console.WriteLine($"current day: {day}");
+
+int month = currentDateTime.Month;
+Console.WriteLine($"current month: {month}");
+
+var dayOfWeek = currentDateTime.DayOfWeek;
+Console.WriteLine($"dayOfWeek: {dayOfWeek}");
+
+int year = DateTime.Now.Year;
+Console.WriteLine($"current year: {year}");
+
+DateTime firstDateTime = new DateTime(2000, 2, 3, 15, 55, 20);
+DateTime secondDateTime = new DateTime(2024, 5, 3, 13, 45, 20);
+
+//difference is of type TimeSpan
+var difference = secondDateTime - firstDateTime;
+Console.WriteLine(difference);
+
+int days = difference.Days;
+int hours = difference.Hours;
+int minutes = difference.Minutes;
+Console.WriteLine($"Days {days} Hours: {hours} Minutes: {minutes}");
+
+//get the date and time 5 days from now
+DateTime fiveDaysAhead = DateTime.Now.AddDays(5);
+Console.WriteLine(fiveDaysAhead);
+
+//get the date time 5 days and 5 hours from now
+DateTime fiveDaysAndfiveHoursFromNow = DateTime.Now.AddDays(5).AddHours(5);
+//DateTime fiveDaysAndfiveHoursFromNow = fiveDaysAhead.AddHours(5);
+Console.WriteLine(fiveDaysAndfiveHoursFromNow);
+
+//get the date and time 2 years and 3 months ago
+
+DateTime twoYearsAndThreeMonthsAgo = DateTime.Now.AddYears(-2).AddMonths(-3);
+Console.WriteLine(twoYearsAndThreeMonthsAgo);
+
+//get date and time 3 years and 1 month ago
+DateTime threeYearsAndOneMonthAgo = DateTime.Now.AddYears(-3).AddMonths(-1);
+Console.WriteLine(threeYearsAndOneMonthAgo);
+
+//Formatting datetime to string
+string formattedString = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(formattedString);
+
+string anotherFormattedDate = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+Console.WriteLine(anotherFormattedDate);
+
+string yetAnotherFormattedDate = DateTime.Now.ToString("MMMM dd");
+Console.WriteLine(yetAnotherFormattedDate);
+
+int a = 5;
+string five = a.ToString();
+Console.WriteLine();
+
