118edfd4aedbfa17f57b904848aa94aba3f782ef
Ilija Mitev <ilija_mitev_mk@yahoo.com>
2024-03-12 18:20:36 +0100

    Class 06 init

---
+++ G2/Class06/Class06Code/ClassesRecap/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassesRecap.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+        public Student()
+        {
+            
+        }
+
+        public Student(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Student {GetFullName()} is {Age} years old and studies at the {Academy} in the group {Group}");
+        }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ G2/Class06/Class06Code/ClassesRecap/Program.cs
@@ -0,0 +1,53 @@
+﻿using ClassesRecap.Models;
+
+
+#region Ways to create instances (objects) from class
+// 1) using the empty constructor
+Student petko = new Student();
+petko.Id = 1;
+petko.FirstName = "Petko";
+petko.LastName = "Petkovski";
+petko.Age = 32;
+// petko.PrintInfo();
+
+// 2) using custom defined constructor with parameters
+Student john = new Student(2, "John", "Johnsky");
+// john.PrintInfo();
+
+// 3) using {} to explicitly assign all properties
+Student bob = new Student()
+{
+    Id = 3,
+    FirstName = "Bob",
+    LastName = "Bobsky",
+    Group = "G2",
+    Academy = "Academy of programming",
+    Age = 25
+};
+// bob.PrintInfo();
+
+//Student jane = new Student(4, "Jane", "Doe");
+#endregion
+
+// Create array of 5 students
+Student[] students = new Student[]
+{
+    petko,
+    john,
+    bob,
+    new Student(4, "Jane", "Doe"),
+    new Student()
+    {
+        Id = 5,
+        FirstName = "Mickey",
+        LastName = "Mouse"
+    }
+};
+
+// Print students full names in console
+foreach (Student student in students)
+{
+    Console.WriteLine(student.GetFullName());
+}
+
+Console.ReadLine();
