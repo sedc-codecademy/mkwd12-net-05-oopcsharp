26e5a5806c1c1186c027830e6bd5792a2bc7fd4e
tstojanovska <stojanovskatanja03@gmail.com>
2024-03-12 20:49:36 +0100

    Class06

---
+++ G7/Class06/Code/QinshiftAcademy.Class06.RetroExercise/Program.cs
@@ -0,0 +1,62 @@
+﻿using QinshiftAcademy.Class06.RetroExercise;
+
+Student[] students = new Student[5];
+
+students[0] = new Student("Tanja", "Panchevska", "Qinshift", "Group7");
+students[1] = new Student("Aleksandar", "Milovski", "Qinshift", "Group6");
+students[2] = new Student("Marko", "Markovski", "Qinshift", "Group6");
+students[3] = new Student("Ana", "Markovska", "Qinshift", "Group5");
+students[4] = new Student("Jovana", "Petrovska", "Qinshift", "Group8");
+
+foreach(Student student in students)
+{
+    student.PrintInfo();
+}
+
+Console.WriteLine("Please enter a first name to search for");
+string inputName = Console.ReadLine();
+
+Student foundStudent = FindStudent(students, inputName);
+
+if(foundStudent != null)
+{
+    foundStudent.PrintInfo();
+}
+else
+{
+    Console.WriteLine($"Student with firstName {inputName} was not found.");
+}
+
+Student FindStudent(Student[] studentsArray, string firstName)
+{
+    foreach(Student student in studentsArray)
+    {
+        if (inputName.ToLower() == student.FirstName.ToLower())
+        {
+            return student;
+        }
+    }
+
+    return null;
+}
+
+
+//bool isFound = false;
+//foreach(Student student in students) 
+//{
+//    if (inputName.ToLower() == student.FirstName.ToLower())
+//    {
+//        student.PrintInfo();
+//        isFound = true;
+//        break;
+//    }
+//}
+
+//if (!isFound)
+//{
+//    //the student was not found
+//}
+
+
+
+
---
+++ G7/Class06/Code/QinshiftAcademy.Class06.RetroExercise/Student.cs
@@ -0,0 +1,78 @@
+﻿
+
+namespace QinshiftAcademy.Class06.RetroExercise
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+        //the properties of this class should be required
+
+        public Student(string firstName, string lastName, string academy, string group)
+        {
+            //if (string.IsNullOrEmpty(firstName))
+            //{
+            //    Console.WriteLine("FirstName parameter doesn't have a value. A default value will be used.");
+            //    FirstName = "No first name";
+            //}
+            //else
+            //{
+            //    FirstName = firstName;
+            //}
+            FirstName = ValidateInput(firstName, "First Name");
+
+            //if (string.IsNullOrEmpty(lastName))
+            //{
+            //    Console.WriteLine("LastName parameter doesn't have a value. A default value will be used.");
+            //    LastName = "No last name";
+            //}
+            //else
+            //{
+            //    LastName = lastName;
+            //}
+            LastName = ValidateInput(lastName, "Last Name");
+
+            //if (string.IsNullOrEmpty(academy))
+            //{
+            //    Console.WriteLine("Academy parameter doesn't have a value. A default value will be used.");
+            //    Academy = "No academy";
+            //}
+            //else
+            //{
+            //    Academy = academy;
+            //}
+            Academy = ValidateInput(academy, "Academy");
+
+            //if (string.IsNullOrEmpty(group))
+            //{
+            //    Console.WriteLine("Group parameter doesn't have a value. A default value will be used.");
+            //    Group = "No group";
+            //}
+            //else
+            //{
+            //    Group = group;
+            //}
+
+            Group = ValidateInput(group, "Group");
+        }
+
+        private string ValidateInput(string input, string nameOfField)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine($"{nameOfField} parameter doesn't have a value. A default value will be used.");
+                return $"No {nameOfField}";
+            }
+
+            return input;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName} {Academy} {Group}");
+        }
+    }
+}
