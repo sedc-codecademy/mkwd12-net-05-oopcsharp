f122b29b50d8ee2acc786bf7e1ac4e7a8f76864b
Danilo Borozan <daniloborozan07@gmail.com>
2024-03-14 20:49:06 +0100

    add

---
+++ G2/Class07/Qinshift.Class07.Inheritance/Qinshift.Class07.Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Qinshift.Class07.Domain;
+using Qinshift.Class07.Domain.Enums;
+
+Console.WriteLine("Hello, World!");
+
+Employee employee = new Employee()
+{
+    FirstName = "Zlatko",
+    LastName = "Zlatev",
+    Role = Role.Other
+};
+
+employee.PrintInfo();
+
+Manager manager = new Manager("Elon", "Mask", 200);
+manager.PrintInfo();
+manager.AddBonus(100);
+manager.PrintInfo();
