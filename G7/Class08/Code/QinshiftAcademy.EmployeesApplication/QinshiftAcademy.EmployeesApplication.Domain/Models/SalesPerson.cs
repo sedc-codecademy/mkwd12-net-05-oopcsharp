e51a171f55dcc5265bfd02972f31bbdaff7be025
tstojanovska <stojanovskatanja03@gmail.com>
2024-03-19 20:55:15 +0100

    Class 08

---
+++ G7/Class08/Code/QinshiftAcademy.EmployeesApplication/QinshiftAcademy.EmployeesApplication.Domain/Models/SalesPerson.cs
@@ -0,0 +1,44 @@
+﻿
+
+using QinshiftAcademy.EmployeesApplication.Domain.Enums;
+
+namespace QinshiftAcademy.EmployeesApplication.Domain.Models
+{
+    public class SalesPerson : Employee
+    {
+        private double _successSaleRevenue {  get; set; }
+
+        public SalesPerson()
+        {
+            Salary = 500;
+            Role = EmployeeRole.Sales;
+        }
+
+        public SalesPerson(string firstName, string lastName, double revenue) 
+            : base(firstName, lastName, 500, EmployeeRole.Sales)
+        {
+            _successSaleRevenue = revenue;
+        }
+
+        public void AddSuccessRevenue(double successSaleRevenue)
+        {
+            _successSaleRevenue += successSaleRevenue; 
+        }
+
+        public override double GetSalary()
+        {
+            if(_successSaleRevenue < 2000)
+            {
+                return Salary + 500;
+            }
+            else if(_successSaleRevenue >= 2000 && _successSaleRevenue < 5000)
+            {
+                return Salary + 1000;
+            }
+            else
+            {
+                return Salary + 1500;
+            }
+        }
+    }
+}
---
+++ G7/Class08/Code/QinshiftAcademy.EmployeesApplication/QinshiftAcademy.EmployeesApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using QinshiftAcademy.EmployeesApplication.Domain.Enums;
+using QinshiftAcademy.EmployeesApplication.Domain.Models;
+
+Employee employee = new Employee();
+
+Employee secondEmployee = new Employee("Kate", "Katesky", 1000, EmployeeRole.Manager);
+secondEmployee.PrintInfo();
+
+double secondEmployeeSalary = secondEmployee.GetSalary();
+
+Manager manager = new Manager("Paul", "Paulsky", 1000);
+manager.AddBonus(200);
+
+double managerSalary = manager.GetSalary();
