9c7cc27e180368fdcccf914b4fcc84e485115a6f
Danilo Borozan <daniloborozan07@gmail.com>
2024-03-21 22:01:15 +0100

    add code

---
+++ G2/Class09/Qinshift.Exception/Qinshift.Domain/CustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qinshift.Domain
+{
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+
+        }
+
+        public CustomException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ G2/Class09/Qinshift.Exception/Qinshift.Domain/Human.cs
@@ -0,0 +1,8 @@
+﻿namespace Qinshift.Domain
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
